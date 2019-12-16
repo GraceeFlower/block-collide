2b1b23452c7857e35c93b5cf66178312c48a5123
GraceeFlower <1491644103@qq.com>
2019-12-16 13:39:38 +0800

    [WangMin] feat: change the static block's color when it collides the moving block

--- index.js
+++ index.js
@@ -41,6 +41,16 @@ var oArea = document.getElementsByClassName("moveable-area")[0];
       oMove.style.left = movedX + "px";
       oMove.style.top = movedY + "px";
 
+      changeColor(oStatic.offsetLeft, oStatic.offsetTop);
+    }
+  }
+
+  function changeColor(left, top) {
+    if (oMove.offsetTop >= top - 80 && oMove.offsetTop <= top + 80
+       && oMove.offsetLeft >= left - 80 && oMove.offsetLeft <= left + 80) {
+        oStatic.style.backgroundColor = "#1c43f0";
+      } else {
+        oStatic.style.backgroundColor = "#f0f146";
     }
   }
 
